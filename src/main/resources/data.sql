3c247d9e47d8c0a885adbc117c93bb9cfb3d3bc7
rafaeln2 <rafaeln_nascimento@hotmail.com>
2024-07-17 23:51:43 -0300

    adicionado data.sql pra pre-insert de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.users
+("password", username)
+VALUES('555', 'rafa');
+INSERT INTO public.users
+("password", username)
+VALUES('123', 'boss');
+
+INSERT INTO public.task
+(description, user_id)
+VALUES('tarefa 999 teste', 1);
+INSERT INTO public.task
+(description, user_id)
+VALUES('tarefa 123 teste', 1);
+INSERT INTO public.task
+(description, user_id)
+VALUES('tarefa 736 teste', 1);

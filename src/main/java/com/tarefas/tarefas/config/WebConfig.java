5bd199b5ab7b4b313771d98fde1a617b64ba340a
rafaeln2 <rafaeln_nascimento@hotmail.com>
2024-07-18 21:46:40 -0300

    adicionado arquivo de configuracao cors

---
+++ src/main/java/com/tarefas/tarefas/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.tarefas.tarefas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){ //libera acesso pra outras aplicacoes acessarem as rotas
+        registry.addMapping("/**");
+    }
+}

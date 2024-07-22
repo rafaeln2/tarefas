153bcd4c836629e8b021943cf069ec4d21f2e462
rafaeln2 <rafaeln_nascimento@hotmail.com>
2024-07-22 00:06:28 -0300

    ajustes tela

--- view/script.js
+++ view/script.js
@@ -4,4 +4,33 @@ function hideLoader(){
     document.getElementById("loading").style.display = "none";
 }
 
-hideLoader();
+function show(tasks){
+    let tab = ` <thead>
+                <th scope="col">#</th>
+                <th scope="col">Descriptionth>
+                <th scope="col">Username</th>
+                <th scope="col">User Id</th>
+            </thead>`;
+    for (let task in tasks) {
+        tab += `
+            <tr>
+                <td>${task.id}</td>
+                <td>${task.description}</td>
+                <td>${task.user.username}</td>
+                <td>${task.user.id}</td>
+            </tr>
+        `
+    }
+    document.getElementById("tasks").innerHTML = tab;
+}
+
+async function getAPI(url){
+    const response = await fetch(url, {method:"GET"})
+
+    var data = await response.json();
+    console.log(data)
+    if(response) hideLoader();
+    show(data)
+}
+
+getAPI(url);
